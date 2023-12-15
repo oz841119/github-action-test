10a9c20b9c6966491bb4e6fff4744b48eefb52e5
Q_Q <oz841119@gmail.com>
2023-12-15 09:45:33 +0800

    none

--- __spec__/utils/inputCheatCode.spec.ts
+++ __spec__/utils/inputCheatCode.spec.ts
@@ -1,8 +1,8 @@
 import inputCheatCode from '../../src/utils/inputCheatCode'
 describe('utils: inputCheatCode: branch coverage = 50', () => {
-//   test('input "GIVEMEAAPPLE"', () => {
-//     expect(inputCheatCode('GIVEMEAAPPLE')).toBe(undefined)
-//   })
+  test('input "GIVEMEAAPPLE"', () => {
+    expect(inputCheatCode('GIVEMEAAPPLE')).toBe(undefined)
+  })
   test('input "GETMONEY"', () => {
     expect(inputCheatCode('GETMONEY')).toBe('+$1000000')
   })
